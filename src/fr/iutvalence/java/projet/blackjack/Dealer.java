4bf7ecdb35f8f4a014c8c95d3e6bd5836989bd11
DuboisT <thomas.dubois@iut-valence.fr>
2015-04-30 15:34:38 +0200

    created class Dealer with empty method

---
+++ src/fr/iutvalence/java/projet/blackjack/Dealer.java
@@ -0,0 +1,28 @@
+package fr.iutvalence.java.projet.blackjack;
+
+public class Dealer
+{
+	public Dealer() {
+	}
+	
+	public void dealPlayersHand()
+	{
+		
+	}
+	
+	public void dealDealersHand()
+	{
+		
+	}
+	
+	public void giveCard()
+	{
+		
+	}
+	
+	public int payInsurance()
+	{
+		return 0;
+	}
+	
+}
