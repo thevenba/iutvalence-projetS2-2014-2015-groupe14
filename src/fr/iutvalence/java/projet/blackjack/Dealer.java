7cdf0f1d28603d99aaa1365b58e0d9a6b8ed731a
DuboisT <thomas.dubois@iut-valence.fr>
2015-04-30 15:45:50 +0200

    dealer class with empty method

--- src/fr/iutvalence/java/projet/blackjack/Dealer.java
+++ src/fr/iutvalence/java/projet/blackjack/Dealer.java
@@ -1,10 +1,20 @@
 package fr.iutvalence.java.projet.blackjack;
 
+/**
+ * @author duboitho
+ *	class dealer the dealer deal the cards and is the player competitor
+ */
 public class Dealer
 {
+	/**
+	 * constructor of dealer
+	 */
 	public Dealer() {
 	}
 	
+	/**
+	 * deal the player's hand
+	 */
 	public void dealPlayersHand()
 	{
 		
