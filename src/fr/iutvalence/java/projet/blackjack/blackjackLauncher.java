6cb49dc16f402130d7f468fcfa2030ca8927e387
thevenba <baptiste.thevenet@iut-valence.fr>
2015-04-30 14:28:35 +0200

    Init commit

---
+++ src/fr/iutvalence/java/projet/blackjack/blackjackLauncher.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package fr.iutvalence.java.projet.blackjack;
+
+/**
+ * @author thevenba
+ *
+ */
+public class blackjackLauncher {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
