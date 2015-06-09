2fe3f2fa829ce1047bdcc54512a9dfa077d4a88e
duboitho <duboitho@pc-b123-04.iut-valence.fr>
2015-06-09 17:52:42 +0200

    commit 09/06/2015 17:51 split PlayerHandPanel

---
+++ src/fr/iutvalence/java/projet/blackjackHCI/MainHandPanel.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+package fr.iutvalence.java.projet.blackjackHCI;
+
+import java.util.List;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JSplitPane;
+
+import fr.iutvalence.java.projet.blackjack.Card;
+import fr.iutvalence.java.projet.blackjack.Player;
+
+/**
+ * @author duboitho
+ *
+ */
+public class MainHandPanel extends JPanel
+{
+	
+	private Player player;
+	private List<Card> mainHand;	
+	private JButton mainButton = new JButton();
+	
+	public MainHandPanel(Player player){
+		
+		this.player=player;
+		this.mainHand=this.player.getMainHand().getCards();
+		
+		for(int i=0;i<mainHand.size();i++)
+		{
+			JButton button = new JButton(""+this.player.getMainHand().getCards().get(i));
+			this.add(button);
+			button.setEnabled(true);
+		}
+	}
+	
+	public void refreshPlayerHand(){
+		this.mainHand=this.player.getMainHand().getCards();
+		this.mainButton.setText(""+this.player.getMainHand().getCards().get(mainHand.size()));
+		this.add(mainButton);
+	}
+}
